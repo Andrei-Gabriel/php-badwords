09720a828b60c42f1a67578a884017bd29d1eab8
Andrei-Gabriel <andrescu.gabriel.andrei@gmail.com>
2022-01-17 17:14:29 +0100

    Primo problema con PHP

---
+++ PHP_BadWords.php
@@ -0,0 +1,17 @@
+<?php 
+    $ptext = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus est iusto minima molestias non error quisquam, molestiae aliquam enim, laudantium accusantium at nostrum officia? Architecto cupiditate perspiciatis repellat consequuntur nobis!";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Bad Words</title>
+</head>
+<body>
+    <h2>Explicit</h2>
+    <p><?php echo$pText ?></p>
+</body>
+</html>
